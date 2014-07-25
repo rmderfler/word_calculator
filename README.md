ee4b52c72d0a3f572eb31a24c086b913689ae5fd
Tanner Steward & Michael Derfler <student@epicodus.com>
2014-07-25 12:24:11 -0700

    solved problem without using eval

---
+++ README.md
@@ -0,0 +1,10 @@
+WORD CALCULATOR
+
+This program lets you type out a calculation you want done in sentence form and retuns your answer.
+
+Instructions:
+Type a calculation out in sentence form. 
+i.e. "What is 2 plus 2?"
+
+Created by: Michael Derfler and Tanner Steward
+
--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -1,18 +1,26 @@
 def word_calculator(string)
-
-  array = string.split(" ")
+  array = string.split("?")
+  array = string.split("")
+  array.map! {|x| x == "?" ? "" : x}.flatten!
+  array = array.join("")
+  array = array.split(" ")
 
   array.map! {|x| x == "plus" ? "+" : x}.flatten!
   array.map! {|x| x == "minus" ? "-" : x}.flatten!
   array.map! {|x| x == "times" ? "*" : x}.flatten!
   array.map! {|x| x == "divided" ? "/" : x}.flatten!
   array.map! {|x| x == "power" ? "**" : x}.flatten!
+
   input_keep = array.keep_if {|v| v =~ /[\*\/+-0123456789]/}
   input_keep = input_keep.join("")
-  result = eval(input_keep)
+
+  i = input_keep[0]
+  j = input_keep[2]
+  i, op, j = input_keep.scan(/(\d+)([+\-*\/])(\d+)/)[0]
+  result =i.to_i.send op, j.to_i
   result
 end
-
+puts word_calculator("What is 2 plus 3?")
 
 
 
