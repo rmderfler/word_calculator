4ac7af9438558b8b859266109890bb9bbb421e9f
Tanner Steward & Michael Derfler <student@epicodus.com>
2014-07-25 16:40:37 -0700

    works without eval statment

--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -12,15 +12,90 @@ def word_calculator(string)
   array.map! {|x| x == "power" ? "**" : x}.flatten!
 
   input_keep = array.keep_if {|v| v =~ /[\*\/+-0123456789]/}
-  input_keep = input_keep.join("")
-
   i = input_keep[0]
+  op = input_keep[1]
   j = input_keep[2]
-  i, op, j = input_keep.scan(/(\d+)([+\-*\/])(\d+)/)[0]
-  result =i.to_i.send op, j.to_i
-  result
+  two = input_keep[3]
+  k = input_keep[4]
+puts op
+  if op == "**" && two == "**"
+    puts "HI"
+    result = i.to_i**j.to_i**k.to_i
+
+  elsif op == "**" && two == "+"
+    result = i.to_i**j.to_i+k.to_i
+
+  elsif op == "**" && two == "-"
+    result = i.to_i**j.to_i-k.to_i
+
+  elsif op == "**" && two == "*"
+    result = i.to_i**j.to_i*k.to_i
+
+  elsif op == "**" && two == "/"
+    result = i.to_i**j.to_i/k.to_i
+
+  elsif op == "+" && two == "**"
+    result = j.to_i**k.to_i+i.to_i
+
+  elsif op == "-" && two == "**"
+    result = j.to_i**k.to_i-i.to_i
+
+  elsif op == "*" && two == "**"
+    result = j.to_i**k.to_i*i.to_i
+
+  elsif op == "/" && two == "**"
+    result = j.to_i**k.to_i/i.to_i
+
+  elsif op == "*" && two == "+"
+    result = i.to_i*j.to_i+k.to_i
+
+  elsif op == "+" && two == "*"
+    result = j.to_i*k.to_i+i.to_i
+
+  elsif op == "*" && two == "-"
+    result = i.to_i*j.to_i-k.to_i
+
+  elsif op == "-" && two == "*"
+    result = j.to_i*k.to_i-i.to_i
+
+  elsif op == "/" && two == "+"
+    result = i.to_i/j.to_i+k.to_i
+
+  elsif op == "+" && two == "/"
+    result = j.to_i/k.to_i+i.to_i
+
+  elsif op == "/" && two == "-"
+    result = i.to_i/j.to_i-k.to_i
+
+  elsif op == "-" && two == "/"
+    result = j.to_i/k.to_i-i.to_i
+
+  elsif op == "+"
+    result = i.to_i+j.to_i
+
+  elsif op == "-"
+    result = i.to_i-j.to_i
+
+  elsif op == "*"
+    result = i.to_i*j.to_i
+
+  elsif op == "/"
+    result = i.to_i/j.to_i
+
+  elsif op == "**"
+    result = i.to_i**j.to_i
+
+  else
+    input_keep = input_keep.join("")
+    i, op, j, two, k = input_keep.scan(/(\d+)([+\-*\/])(\d+)(?:([+\-*\/])(\d+))?/)[0]
+    result = i.to_i.send op, j.to_i
+    result
+    result = result.to_i.send two, k.to_i
+  end
+
+
 end
-puts word_calculator("What is 2 plus 3?")
+puts word_calculator("What is 4 plus 2  ")
 
 
 
